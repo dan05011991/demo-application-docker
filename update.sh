464d775856710d6446cf18d7c1026f92da23e4cf
Dan Foley <>
2020-04-08 17:35:40 +0100

    Test

--- update.sh
+++ update.sh
@@ -2,6 +2,10 @@ IMAGE=$1
 COMPOSE_FILE=$2
 PROJECT_DIR=$3
 
+echo $IMAGE
+echo $COMPOSE_FILE
+echo $PROJECT_DIR
+
 SNAPSHOT=$(mvn -f $PROJECT_DIR/pom.xml -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 
 sed -i -E "s/$IMAGE.+/$IMAGE$SNAPSHOT/" $COMPOSE_FILE
