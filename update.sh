cdc972401489cd6ebf7f5424f8eb1ecef7f8d45c
Dan Foley <>
2020-04-09 12:34:35 +0100

    Updated

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+
 IMAGE=$(echo $1 | sed 's/\//\\\//g')
 COMPOSE_FILE=$2
 PROJECT_DIR=$3
@@ -8,7 +9,7 @@ SNAPSHOT=$(mvn -f $PROJECT_DIR/pom.xml -q -Dexec.executable=echo -Dexec.args='${
 
 sed -i -E "s/$IMAGE.+/$IMAGE$SNAPSHOT/" $COMPOSE_FILE
 
-if [[ $(git diff | wc -l) -gt 0 ]]; then
+if [ $(git diff | wc -l) -gt 0 ]; then
     git add docker-compose.yaml
     git commit -m "New release"
     git push origin $REMOTE_BRANCH
