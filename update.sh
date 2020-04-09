ff73ef2c7cbe626aa327cf4e74149ce285cc9646
Dan Foley <>
2020-04-09 12:10:43 +0100

    Updated

--- update.sh
+++ update.sh
@@ -1,11 +1,15 @@
 IMAGE=$(echo $1 | sed 's/\//\\\//g')
 COMPOSE_FILE=$2
 PROJECT_DIR=$3
+REMOTE_BRANCH=$4
 
-echo $IMAGE
-echo $COMPOSE_FILE
-echo $PROJECT_DIR
 
 SNAPSHOT=$(mvn -f $PROJECT_DIR/pom.xml -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 
 sed -i -E "s/$IMAGE.+/$IMAGE$SNAPSHOT/" $COMPOSE_FILE
+
+if [[ $(git diff | wc -l) -gt 0 ]]; then
+    git add docker-compose.yaml
+    git commit -m "New release"
+    git push origin $REMOTE_BRANCH
+fi
